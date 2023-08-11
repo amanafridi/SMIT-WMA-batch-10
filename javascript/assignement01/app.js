87642d8e9666ee9a64c16ac76147e1bdc7460d0e
Aman Afridi <amanafridi983@gmail.com>
2023-08-11 18:35:45 +0500

    java assignment 1

---
+++ javascript/assignement01/app.js
@@ -0,0 +1,209 @@
+// ---------------------------------chapter01 / Ques.01--------------------------------
+// alert("welcome");
+
+// ---------------------------------chapter01 / Ques.02--------------------------------
+// alert("Error!please enter the correct password");
+
+// ---------------------------------chapter01 / Ques.03--------------------------------
+// alert("welcome to JS land \n happy coding");
+
+// ---------------------------------chapter01 / Ques.04--------------------------------
+// alert("Welcome to JS land")
+// alert("happy coding")
+
+// ---------------------------------chapter01 / Ques.05 --------------------------------
+// alert("hello.....i can can run js through my web browser's console")
+
+// ---------------------------------chapter01 / Ques.06 and 07--------------------------------
+//   Done
+
+
+// ---------------------------------chapter02 / Ques.01--------------------------------
+// var username ;
+
+// ---------------------------------chapter02 / Ques.02--------------------------------
+// var myName = "Amanullah afridi";
+
+// ---------------------------------chapter02 / Ques.03--------------------------------
+// var message = "hello world";
+// alert(message)
+
+// ---------------------------------chapter02 / Ques.04--------------------------------
+// var fullName ="jhone Doe";
+// var Age = 15;
+// var Course = "certified mobile application development";
+// alert(fullName);
+// alert(Age + "years old");
+// alert(Course);
+
+// ---------------------------------chapter02 / Ques.05--------------------------------
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// ---------------------------------chapter02 / Ques.06--------------------------------
+// var email = "amanafridi983@gmail.com";
+// alert("My email address is " + email)
+
+// ---------------------------------chapter02 / Ques.07--------------------------------
+// var book = " A smarter way to learn javascript";
+// alert("i am trying to learn from a book " + book);
+
+// ---------------------------------chapter02 / Ques.08--------------------------------
+// var para = "yah! i can write HtML content through Javascript"
+// console.log(para);
+
+// ---------------------------------chapter02 / Ques.09--------------------------------
+// alert("▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬");
+
+
+
+// ---------------------------------chapter03 / Ques.01--------------------------------
+// var age = 23;
+// alert("i am " + age + "year old");
+
+// ---------------------------------chapter03 / Ques.02--------------------------------
+// var visit= 14;
+// alert("You have visited this site "+visit+" times")
+
+// ---------------------------------chapter03 / Ques.03--------------------------------
+// var birthYear=1998;
+// console.log("My birth year is "+birthYear);
+
+// ---------------------------------chapter03 / Ques.04--------------------------------
+// var fullName = "Jhon Doe";
+// var productTitle = "T-shirt";
+// var Quantity = 5;
+// console.log ( fullName + " order " + Quantity + " " + productTitle + " XYZ clothing store" );
+
+
+// ---------------------------------chapter04 / Ques.01--------------------------------
+// var fullName , age , contact;
+
+// ---------------------------------chapter04 / Ques.02--------------------------------
+// Legal:
+// userResponse
+// userResponseTime 
+// userResponseTimeLimit 
+// respons
+// fulName
+
+// Illegal:
+// var 
+// alert
+// console
+// log
+
+// ---------------------------------chapter04 / Ques.04--------------------------------
+// fill in the blanks
+
+// ---------------------------------chapter05 / Ques.01--------------------------------
+// var num = 3;
+// var numTwo = 5;
+// var total = num + numTwo ;
+// console.log("Sum of " + num + " and " + numTwo + " is " + total);
+
+// ---------------------------------chapter05 / Ques.02--------------------------------
+// Sub
+// var num = 3;
+// var numTwo = 5;
+// var total = num - numTwo ;
+// console.log("Sum of " + num + " and " + numTwo + " is " + total);
+
+// MUltiplication
+// var num = 3;
+// var numTwo = 5;
+// var total = num * numTwo ;
+// console.log("Sum of " + num + " and " + numTwo + " is " + total);
+
+//divison
+// var num = 3;
+// var numTwo = 5;
+// var total = num / numTwo ;
+// console.log("Sum of " + num + " and " + numTwo + " is " + total);
+
+// ---------------------------------chapter05 / Ques.03--------------------------------
+// var num;
+// console.log("value after variable declaration is: " +num);
+// num = 5;
+// console.log("Initial value: " +num);
+// ++num;
+// console.log("value after Increment is: " +num);
+// var numTwo = num + 7;
+// console.log("value after addition  is: " +numTwo);
+// --numTwo;
+// console.log("value after decrement is: " +numTwo);
+// var numThree = numTwo % 3;
+// console.log("the reminder is: " +numThree);
+
+// ---------------------------------chapter05 / Ques.04--------------------------------
+
+// var cost = 600;
+// var person=5;
+// var total= cost * person;
+// console.log("Total cost of buying "+person+" tickets to a movie is "+total+"PKR");
+
+
+// ---------------------------------chapter05 / Ques.05--------------------------------
+// var table=4;
+// var times=1;
+
+// var total=table*times;
+
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+// times++;
+// var total=table*times;
+// console.log(table+"x"+times+"="+total);
+
+// ---------------------------------chapter05 / Ques.06--------------------------------
+//  var celsius = 25;
+//  var c = (celsius*9/5)+32;
+//  console.log(celsius + "\u00B0 C is " + c + " \u00B0 F");
+ 
+ 
+ 
+//  var fahrenheit = 70;
+//  var f = (fahrenheit-32)*5/9;
+//  console.log(fahrenheit + "\u00B0 F is " + f + " \u00B0 C");
+
+
+// ---------------------------------chapter05 / Ques.07--------------------------------
+// var firstItem = 300;
+// var secondItem = 500;
+// var firstQuantity = 2;
+// var secondQuantity = 3;
+// var shipping = 100;
+
+// var firstTotal = firstItem * firstQuantity ;
+// var secondTotal = secondItem * secondQuantity;
+// var total = firstTotal + secondTotal + shipping ;
+// console.log("price of first item is "+ firstItem);
+// console.log("Quantity of item 1 is "+ firstQuantity );
+// console.log("price of second item is "+ secondItem);
+// console.log("Quantity of item 2 is "+ secondQuantity);
+// console.log("shipping charges "+ shipping);
+// console.log("Total cost of your order is "+ total);
+
+// ---------------------------------chapter05 / Ques.08--------------------------------
