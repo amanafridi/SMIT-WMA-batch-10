90824af342ffa480b4da4c2c5dbd26ed3b7d366a
Aman Afridi <amanafridi983@gmail.com>
2023-08-16 23:05:13 +0500

    update assignment 2 javascript

--- javascript/assignment02/app.js
+++ javascript/assignment02/app.js
@@ -1,1 +1,35 @@
-//***********************chap # 09--question # 01**********************
+//***********************chap # 6-9  question # 01**********************
+
+// var num = 10;
+// console.log("Result : \n the value of a is: " +num ,"\n -----------------")
+
+// ++num;
+// console.log("the value of num is: "+num)
+// console.log("Now the value of num is: "+num)
+
+// console.log("the value of num is: "+num)
+// num++;
+// console.log("Now the value of num is: "+num)
+
+// --num;
+// console.log("the value of num is: "+num)
+// console.log("Now the value of num is: "+num)
+
+// console.log("the value of num is: "+num)
+// num--;
+// console.log("Now the value of num is: "+num)
+
+
+//***********************chap # 6-9  question # 02**********************
+
+// var a = 1 , b = 2;
+// var result = --a - --b + ++b + b--;
+
+// console.log("The result is "+result);
+ 
+// --a; 0
+// --a - --b; 1
+// --a - --b + ++b; 2
+// --a - --b + ++b + b--; 3
+
+//***********************chap # 6-9  question # 03**********************
