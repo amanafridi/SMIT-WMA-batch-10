264284ec42e95ffc9157bd0492f7b08d171b905a
Aman Afridi <amanafridi983@gmail.com>
2023-08-18 21:53:03 +0500

    update javascript assignment 2

--- javascript/assignment02/app.js
+++ javascript/assignment02/app.js
@@ -73,26 +73,137 @@
 
 //***********************chap # 6-9  question # 05**********************
 
-var subOne = prompt("Enter first subject");
-var sub1Obtained = +prompt("first subject obtained marks")
-var subTwo = prompt("Enter second subject");
-var sub2Obtained = +prompt("second subject obtained marks")
-var subThree = prompt("Enter Third subject");
-var sub3Obtained = +prompt("Third subject obtained marks")
-var totalMarks = 100;
+// var subOne = prompt("Enter first subject");
+// var sub1Obtained = +prompt("first subject obtained marks")
+// var subTwo = prompt("Enter second subject");
+// var sub2Obtained = +prompt("second subject obtained marks")
+// var subThree = prompt("Enter Third subject");
+// var sub3Obtained = +prompt("Third subject obtained marks")
+// var totalMarks = 100;
 
-var percentage1 = (sub1Obtained/totalMarks)*100;
-var percentage2 = (sub2Obtained/totalMarks)*100;
-var percentage3 = (sub3Obtained/totalMarks)*100;
+// var percentage1 = (sub1Obtained/totalMarks)*100;
+// var percentage2 = (sub2Obtained/totalMarks)*100;
+// var percentage3 = (sub3Obtained/totalMarks)*100;
 
-console.log("Subjects  Total Marks  Obtained Marks  Percentage");
-console.log(subOne+ " " +totalMarks+ " " +sub1Obtained+ " " +percentage1+"%");
-console.log(subTwo+ " " +totalMarks+ " " +sub2Obtained+ " " +percentage1+"%");
-console.log(subThree+ " " +totalMarks+ " " +sub3Obtained+ " " +percentage1+"%");
+// console.log("Subjects  Total Marks  Obtained Marks  Percentage");
+// console.log(subOne+ " " +totalMarks+ " " +sub1Obtained+ " " +percentage1+"%");
+// console.log(subTwo+ " " +totalMarks+ " " +sub2Obtained+ " " +percentage1+"%");
+// console.log(subThree+ " " +totalMarks+ " " +sub3Obtained+ " " +percentage1+"%");
 
-var totalSubjectMarks = totalMarks*3;
-var totalObtainedMarks = sub1Obtained + sub2Obtained + sub3Obtained;
-var totalpercentage = percentage1 + percentage2 + percentage3/100;
-console.log("        "+totalSubjectMarks +" " + totalObtainedMarks+" " + totalpercentage+"%");
+// var totalSubjectMarks = totalMarks*3;
+// var totalObtainedMarks = sub1Obtained + sub2Obtained + sub3Obtained;
+// var totalpercentage = percentage1 + percentage2 + percentage3/100;
+// console.log("        "+totalSubjectMarks +" " + totalObtainedMarks+" " + totalpercentage+"%");
+
+//***********************chap # 9-11  question # 01**********************
+
+// var city = prompt("what is your city name")
+// if(city === "Karachi"){
+//     console.log("welcome to the city of light")
+// }else{
+//     console.log("Congrats you live in darkness")
+// }
+
+//***********************chap # 9-11  question # 02**********************
+
+// var gender = prompt("Enter the gender")
+// if(gender === "Male"){
+//     console.log("Good morning Sir")
+// } else if(gender === "Female")
+// {
+//     console.log("Good morning Ma'am")
+// }else{
+//     console.log("incorrect input")
+// }
+
+//***********************chap # 9-11  question # 03**********************
+
+// var colors = prompt("enter the color")
+// if(colors === "Red"){
+//     console.log("Must stop")
+// }else if (colors === "Yellow"){
+//     console.log("ready to move")
+// }else if(colors === "Green"){
+//     console.log("Move now")
+// }else{
+//     console.log("stoped")
+// }
+
+//***********************chap # 9-11  question # 04**********************
+
+// var fuel = +prompt("enter the fuel in liter")
+// if(fuel < 0.25){
+//     console.log("“Please refill the fuel in your car")
+// }else{
+//     console.log("No need")
+// }
+
+//***********************chap # 9-11  question # 05**********************
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+//***********************chap # 9-11  question # 06**********************
+
+// var subOne=+prompt("english")
+// var subTwo= +prompt("urdu")
+// var subThree=+prompt("math")
+// var totalMarks= 100;
+// var percentage = (subOne + subTwo + subThree *100 )/totalMarks;
+// console.log("Percentage: " +percentage+ " %");
+// if(percentage >=80){
+//     console.log("A-One")
+//     console.log("Excellent")
+// }else if (percentage >= 70){
+//     console.log("B")
+//     console.log("Good")
+// }else if (percentage >= 60){
+//     console.log("C")
+//     console.log("You need to Improve")
+// }else if(percentage < 60){
+//     console.log("Fail")
+//     console.log("Sorry")
+// }else {
+//     console.log("Invalid input")
+// }
+
+
+//***********************chap # 9-11  question # 07**********************
+
+
 
 
